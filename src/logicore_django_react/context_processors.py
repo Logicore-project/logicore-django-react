2176fe8ded36953238bee5559a832430e3cb0018
Andrew Boltachev <andrewboltachev@gmail.com>
2022-09-11 22:26:33 +0300

    develop full-featured react tags

--- src/logicore_django_react/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-
-def react(request):
-    return {
-        'FRONTEND_DEV_MODE': settings.get("FRONTEND_DEV_MODE", False),
-        "test": "2-hf-13hf340=34hj034",
-    }
