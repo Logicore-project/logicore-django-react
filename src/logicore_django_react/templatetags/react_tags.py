fae49fcd41bcebc392bf7172f19245e87f4ad550
Andrew Boltachev <andrewboltachev@gmail.com>
2022-09-11 22:33:29 +0300

    logicore_django_react dev

--- src/logicore_django_react/templatetags/react_tags.py
+++ src/logicore_django_react/templatetags/react_tags.py
@@ -9,9 +9,9 @@ register = template.Library()
 
 @register.inclusion_tag("logicore_django_react/include_react_css.html")
 def include_react_css(context):
-    return {"FRONTEND_DEV_MODE": settings.FRONTEND_DEV_MODE}
+    return {"FRONTEND_DEV_MODE": getattr(settings, "FRONTEND_DEV_MODE", False)}
 
 
 @register.inclusion_tag("logicore_django_react/include_react_js.html")
 def include_react_js(context):
-    return {"FRONTEND_DEV_MODE": settings.FRONTEND_DEV_MODE}
+    return {"FRONTEND_DEV_MODE": getattr(settings, "FRONTEND_DEV_MODE", False)}
--- src/logicore_django_react/templatetags/react_tags_dev.py
+++ src/logicore_django_react/templatetags/react_tags_dev.py
@@ -9,13 +9,8 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def include_react_dev(context):
-    if not settings.FRONTEND_DEV_MODE:
+    if not getattr(settings, "FRONTEND_DEV_MODE", False):
         return ''
     return SafeString(
         ' '.join(re.findall(r'<script [^>]+></script>', requests.get('http://localhost:3000/').text.replace('/static/', '/react-static/')))
-    if not settings.FRONTEND_DEV_MODE:
-        return ''
-    return SafeString(
-        ' '.join(re.findall(r'<script [^>]+></script>', requests.get('http://localhost:3000/').text.replace('/static/', '/react-static/')))
-    )
     )
