80cd78fcde6c3445b846e7969ad1b853b72c1c57
Andrew Boltachev <andrewboltachev@gmail.com>
2022-09-12 16:56:38 +0300

    Add README

--- src/logicore_django_react/urls.py
+++ src/logicore_django_react/urls.py
@@ -4,17 +4,17 @@ from django.conf import settings
 from . import views
 
 
-urlpatterns = [
+bottom = [
     re_path(r'^(?P<path>.*)$', views.HomeView.as_view()),
 ]
 
 
 if getattr(settings, "FRONTEND_DEV_MODE", False):
-    urlpatterns = [
+    top = [
         re_path(r'^(?P<path>.*\.hot-update\.(js|json))$', views.hot_update), # \.[0-9a-z]{20}
         re_path('^react-static/(?P<path>.+)$', views.react_static),
-    ] + urlpatterns
+    ]
 else:
-    urlpatterns = [
+    top = [
         static("/react-static/", document_root=settings.BASE_DIR / "frontend" / "build" / "react-static")
-    ] + urlpatterns
+    ]
--- src/logicore_django_react/views.py
+++ src/logicore_django_react/views.py
@@ -4,9 +4,15 @@ from django.conf import settings
 
 
 class HomeView(TemplateView):
-    template_name = "home.html"
+    template_name = getattr(
+        settings,
+        "LOGICORE_DJANGO_REACT_TEMPLATE",
+        "logicore_django_react/home.html"
+    )
+
 
 port = getattr(settings, "LOGICORE_DJANGO_REACT_PORT", 3000)
+
 
 def react_static(request, path):
     if path.lower().endswith(".css") or path.lower().endswith(".js"):
